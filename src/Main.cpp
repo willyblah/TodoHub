d6f0abc29972128ef0e4dd93a774f2cb931e1bee
willyblah <williamxu303@outlook.com>
2024-11-19 19:41:12 +0800

    Add basic actions

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <string>
 
 #include "Task.h"
+#include "TodoList.h"
 
 int main() {
     using namespace std;
 
-    cout << "Hello world" << endl;
+    TodoList todoList;
 
-    return 0;
+    while (true) {
+        TodoList::ClearTerminal();
+        cout << "TODO HUB 1.0.0\n";
+        cout << "--------------\n\n";
+
+        todoList.DisplayTasks();
+
+        bool actionDone = false;
+        while (!actionDone) {
+            cout << "\n> ";
+            std::string commandStr;
+            cin >> commandStr;
+            TodoList::Commands command = todoList.StrToCommand(commandStr);
+            switch (command) {
+                case TodoList::Commands::ADD: {
+                    std::string description;
+                    cin.get();
+                    getline(cin, description);
+                    todoList.AddTask({description, false});
+                    actionDone = true;
+                    break;
+                }
+                case TodoList::Commands::QUIT:
+                    return 0;
+                case TodoList::Commands::UNKNOWN:
+                    cerr << "Invalid command.";
+                    break;
+            }
+        }
+    }
 }
