bb1df60378bfe6fb21896c6f05e0bf9744a2ea4f
willyblah <williamxu303@outlook.com>
2024-12-05 16:21:33 +0800

    Add cyan color

--- include/Colors.h
+++ include/Colors.h
@@ -9,7 +9,8 @@ const std::string DEFAULT = "\033[0m";
 const std::string BOLD = "\033[37;1m";
 const std::string RED = "\033[1;31m";
 const std::string GREEN = "\033[1;32m";
-const std::string BLUE = "\033[1;34m";
 const std::string YELLOW = "\033[1;33m";
+const std::string BLUE = "\033[1;34m";
+const std::string CYAN = "\033[1;36m";
 
 #endif  // COLORS_H
